edf367487dee5d6894565a126da4a9dd8eb2aabe
Vitaliy Filippov <vitalif@yourcmc.ru>
2012-11-16 16:47:03 +0400

    Import latest Wikimedia SVN version
    http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/WhoIsWatching@115780
    
    Change-Id: Ib31c75960c5ee1a7deb0d46f725efbe14dbf5e37

---
+++ WhoIsWatching.alias.php
@@ -0,0 +1,237 @@
+<?php
+/**
+ * Aliases for special pages
+ *
+ */
+
+$specialPageAliases = array();
+
+/** English (English) */
+$specialPageAliases['en'] = array(
+	'WhoIsWatching' => array( 'WhoIsWatching' ),
+);
+
+/** Arabic (العربية) */
+$specialPageAliases['ar'] = array(
+	'WhoIsWatching' => array( 'من_يراقب' ),
+);
+
+/** Egyptian Spoken Arabic (مصرى) */
+$specialPageAliases['arz'] = array(
+	'WhoIsWatching' => array( 'مين_بيراقب' ),
+);
+
+/** Banjar (Bahasa Banjar) */
+$specialPageAliases['bjn'] = array(
+	'WhoIsWatching' => array( 'Siapa_nang_maitihi' ),
+);
+
+/** Breton (brezhoneg) */
+$specialPageAliases['br'] = array(
+	'WhoIsWatching' => array( 'PivZoSellet' ),
+);
+
+/** Bosnian (bosanski) */
+$specialPageAliases['bs'] = array(
+	'WhoIsWatching' => array( 'KoPregleda' ),
+);
+
+/** German (Deutsch) */
+$specialPageAliases['de'] = array(
+	'WhoIsWatching' => array( 'Wer_beobachtet' ),
+);
+
+/** Zazaki (Zazaki) */
+$specialPageAliases['diq'] = array(
+	'WhoIsWatching' => array( 'KamoWeyneno' ),
+);
+
+/** Lower Sorbian (dolnoserbski) */
+$specialPageAliases['dsb'] = array(
+	'WhoIsWatching' => array( 'Chto wobglědujo' ),
+);
+
+/** Spanish (español) */
+$specialPageAliases['es'] = array(
+	'WhoIsWatching' => array( 'Quién_esta_vigilando' ),
+);
+
+/** Persian (فارسی) */
+$specialPageAliases['fa'] = array(
+	'WhoIsWatching' => array( 'چه_کسی_پی‌گیری_می‌کند' ),
+);
+
+/** Finnish (suomi) */
+$specialPageAliases['fi'] = array(
+	'WhoIsWatching' => array( 'Ketkä_tarkkailevat' ),
+);
+
+/** French (français) */
+$specialPageAliases['fr'] = array(
+	'WhoIsWatching' => array( 'Qui_a_en_suivi', 'QuiAEnSuivi' ),
+);
+
+/** Franco-Provençal (arpetan) */
+$specialPageAliases['frp'] = array(
+	'WhoIsWatching' => array( 'Qui_at_en_survelyence', 'QuiAtEnSurvelyence' ),
+);
+
+/** Galician (galego) */
+$specialPageAliases['gl'] = array(
+	'WhoIsWatching' => array( 'Quen_está_vixiando' ),
+);
+
+/** Swiss German (Alemannisch) */
+$specialPageAliases['gsw'] = array(
+	'WhoIsWatching' => array( 'Wär_beobachtet?' ),
+);
+
+/** Hebrew (עברית) */
+$specialPageAliases['he'] = array(
+	'WhoIsWatching' => array( 'מי_עוקב' ),
+);
+
+/** Upper Sorbian (hornjoserbsce) */
+$specialPageAliases['hsb'] = array(
+	'WhoIsWatching' => array( 'Štó wobkedźbuje' ),
+);
+
+/** Haitian (Kreyòl ayisyen) */
+$specialPageAliases['ht'] = array(
+	'WhoIsWatching' => array( 'KiyèsKiVeye' ),
+);
+
+/** Hungarian (magyar) */
+$specialPageAliases['hu'] = array(
+	'WhoIsWatching' => array( 'Ki_figyel' ),
+);
+
+/** Interlingua (interlingua) */
+$specialPageAliases['ia'] = array(
+	'WhoIsWatching' => array( 'Qui_observa' ),
+);
+
+/** Indonesian (Bahasa Indonesia) */
+$specialPageAliases['id'] = array(
+	'WhoIsWatching' => array( 'Siapa_yang_memantau', 'SiapaPantau' ),
+);
+
+/** Italian (italiano) */
+$specialPageAliases['it'] = array(
+	'WhoIsWatching' => array( 'ChiOsserva' ),
+);
+
+/** Japanese (日本語) */
+$specialPageAliases['ja'] = array(
+	'WhoIsWatching' => array( 'ページをウォッチしている利用者' ),
+);
+
+/** Khmer (ភាសាខ្មែរ) */
+$specialPageAliases['km'] = array(
+	'WhoIsWatching' => array( 'អ្នកកំពុងតាមដាន' ),
+);
+
+/** Korean (한국어) */
+$specialPageAliases['ko'] = array(
+	'WhoIsWatching' => array( '주시한사용자' ),
+);
+
+/** Colognian (Ripoarisch) */
+$specialPageAliases['ksh'] = array(
+	'WhoIsWatching' => array( 'Wä paß op op', 'WäPassOpOp', 'WäPaßOpOp' ),
+);
+
+/** Cornish (kernowek) */
+$specialPageAliases['kw'] = array(
+	'WhoIsWatching' => array( 'PiwUjiOwKolya' ),
+);
+
+/** Luxembourgish (Lëtzebuergesch) */
+$specialPageAliases['lb'] = array(
+	'WhoIsWatching' => array( 'Wien_iwwerwaacht' ),
+);
+
+/** Lithuanian (lietuvių) */
+$specialPageAliases['lt'] = array(
+	'WhoIsWatching' => array( 'Kas stebi' ),
+);
+
+/** Macedonian (македонски) */
+$specialPageAliases['mk'] = array(
+	'WhoIsWatching' => array( 'КојНабљудува' ),
+);
+
+/** Malayalam (മലയാളം) */
+$specialPageAliases['ml'] = array(
+	'WhoIsWatching' => array( 'ആരൊക്കെശ്രദ്ധിക്കുന്നു' ),
+);
+
+/** Marathi (मराठी) */
+$specialPageAliases['mr'] = array(
+	'WhoIsWatching' => array( 'कोणकोणबघतय' ),
+);
+
+/** Norwegian Bokmål (norsk (bokmål)‎) */
+$specialPageAliases['nb'] = array(
+	'WhoIsWatching' => array( 'Hvem_overvåker' ),
+);
+
+/** Nedersaksisch (Nedersaksisch) */
+$specialPageAliases['nds-nl'] = array(
+	'WhoIsWatching' => array( 'Wee_volgt' ),
+);
+
+/** Dutch (Nederlands) */
+$specialPageAliases['nl'] = array(
+	'WhoIsWatching' => array( 'WieVolgt' ),
+);
+
+/** Polish (polski) */
+$specialPageAliases['pl'] = array(
+	'WhoIsWatching' => array( 'Kto_obserwuje' ),
+);
+
+/** Pashto (پښتو) */
+$specialPageAliases['ps'] = array(
+	'WhoIsWatching' => array( 'څوک يې ګوري' ),
+);
+
+/** Sanskrit (संस्कृतम्) */
+$specialPageAliases['sa'] = array(
+	'WhoIsWatching' => array( 'अन्यकतम्‌पश्यति' ),
+);
+
+/** Slovak (slovenčina) */
+$specialPageAliases['sk'] = array(
+	'WhoIsWatching' => array( 'KtoSleduje' ),
+);
+
+/** Sundanese (Basa Sunda) */
+$specialPageAliases['su'] = array(
+	'WhoIsWatching' => array( 'SahaNuMoncorong' ),
+);
+
+/** Telugu (తెలుగు) */
+$specialPageAliases['te'] = array(
+	'WhoIsWatching' => array( 'ఎవరువీక్షిస్తున్నారు' ),
+);
+
+/** Thai (ไทย) */
+$specialPageAliases['th'] = array(
+	'WhoIsWatching' => array( 'ผู้ที่กำลังเฝ้าดู' ),
+);
+
+/** Tagalog (Tagalog) */
+$specialPageAliases['tl'] = array(
+	'WhoIsWatching' => array( 'Sino_ang_nagbabantay' ),
+);
+
+/** Turkish (Türkçe) */
+$specialPageAliases['tr'] = array(
+	'WhoIsWatching' => array( 'Kimİzliyor' ),
+);
+
+/** Yiddish (ייִדיש) */
+$specialPageAliases['yi'] = array(
+	'WhoIsWatching' => array( 'ווער_פאסט_אויף' ),
+);
