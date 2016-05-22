e5015a5ff822d2099be95f121afe630a81c0a69a
Mark A. Hershberger <mah@nichework.com>
2016-05-21 21:16:18 -0500

    Improve WhoIsWatching extension
    * Use extension.json
    * Add rights so that we can use those instead of globals to control
      access:
      * addpagetoanywatchlist: gets to use the special page to add users.
      * seepagewatchers: gets to see the watchers.
    * Clean up WhoIsWatching::execute() method:
      * Use fewer globals.  Use GlobalVarConfig where possible.
      * Use protected methods for various parts of the execution path.
      * Add stub (WhoIsWatching::eNotifUser()) to later notify editors when
        their watchlists are changed.
      * Add stub (WhoIsWatching::uiNotifyUser()) to later provide better
        feedback to users of this extension.
      * Instead of listing out every user to choose from, provide
        autocomplete for user selection.
      * Instead of showing a confusing "usage" message, provide an
        autocomplete input box so that the user can select a page to
        inspect.
      * Provide slightly better error messages.
      * Refactor to make code more readable.
    * Adapted to changes in core MediaWiki:
      * Remove .i18n.php stub.
      * Moved $wgPageShowWatchingUsers which was removed from core in 1.25
        to $whoiswathing_showwatchingusers
      * Since the message had to be changed anyway, moved the message from
        number_of_watching_users_pageview to whoiswatchingpageview
    * Remove use of sprintf for i18n construction.
    
    Change-Id: I9474771788bbeecbbfa521cfbe1792524d04d301
    (cherry picked from commit dcd0aad156785b00a799ef45b185778da58b95fb)

--- WhoIsWatching_body.php
+++ WhoIsWatching_body.php
@@ -2,92 +2,238 @@
 
 class WhoIsWatching extends SpecialPage {
 
+	protected $targetPage = null;
+	protected $nameType;
+	protected $allowAddingPeople;
+	protected $showWatchingUsers;
+	protected $showIfZero;
+
 	function __construct() {
 		parent::__construct( 'WhoIsWatching' );
+
+		$conf = new GlobalVarConfig( "whoiswatching_" );
+		$user = $this->getUser();
+		$this->nameType = $conf->get( "nametype" );
+		$this->allowAddingPeople = ( !$user->isAnon() && $conf->get( "allowaddingpeople" ) ) ||
+			$user->isAllowed( "addpagetoanywatchlist" );
+		$this->showWatchingUsers = $conf->get( "showwatchingusers" ) ||
+			$user->isAllowed( "seepagewatchers" );
+
 		return true;
 	}
 
 	function execute( $par ) {
-		global $wgRequest, $wgOut, $wgUser;
-		global $whoiswatching_nametype, $whoiswatching_allowaddingpeople;
+		parent::execute( $par );
 
-		$this->setHeaders();
-		$wgOut->setPagetitle( $this->msg( 'whoiswatching' ) );
-
-		$title = $wgRequest->getVal( 'page' );
-		$ns = $wgRequest->getVal( 'ns', '' );
-		if ( $ns !== '' ) {
-			$title = $ns.':'.$title;
-		}
-		$pageTitle = Title::newFromText( $title );
-		if ( !$title || !$pageTitle ) {
-			$wgOut->addWikiMsg( 'specialwhoiswatchingusage' );
-			return;
-		}
-
-		if ( $whoiswatching_allowaddingpeople &&
-			$wgRequest->wasPosted() &&
-			$wgUser->matchEditToken( $wgRequest->getVal( 'token' ) ) ) {
-			$idArray = $wgRequest->getArray( 'idArray' );
-			foreach ( $idArray as $name => $id ) {
-				#$wgOut->addWikiText("* Adding name $name userid $id to watchlist\n");
-				$u = User::newFromId( $id );
-				$u->addWatch( $pageTitle );
+		if ( $this->getTargetPage( $par ) ) {
+			if ( $this->addWatchersForm() ) {
+				return true;
 			}
-			$wgOut->redirect( Title::makeTitle( NS_SPECIAL, 'WhoIsWatching' )->getLocalUrl(
-				array( 'page' => $title )
-			) );
+			$this->showWatchingUsers();
+		}
+	}
+
+	public function checkPermissions() {
+		if ( $this->showWatchingUsers || $this->allowAddingPeople ) {
+			return true;
+		}
+		throw new ErrorPageError(
+			"whoiswatching-permission-denied-title", "whoiswatching-permission-denied",
+			[ $this->getLanguage()->commaList( [ "seepagewatchers", "addpagetoanywatchlist" ] ) ]
+		);
+	}
+
+	protected function getTargetPage( $par ) {
+		$title = $this->getRequest()->getVal( 'page' );
+		if ( !$title && !$par ) {
+			return $this->pickPage();
+		}
+
+		if ( $title ) {
+			$nsRevLookup = array_flip( MWNamespace::getCanonicalNamespaces() );
+			$ns = $this->getRequest()->getVal( 'ns', '' );
+			if ( !ctype_digit( $ns ) ) {
+				$ns = isset( $nsRevLookup[ $ns ] ) ? $nsRevLookup[ $ns ] : null;
+			}
+			$this->targetPage = Title::newFromText( $title, $ns );
+		} else {
+			$this->targetPage = Title::newFromText( $par );
+		}
+
+		if ( !$this->targetPage ) {
+			throw new ErrorPageError( "whoiswatching-usage-title", "specialwhoiswatchingusage" );
+		}
+		$ns = $this->targetPage->getNamespace();
+		if ( $ns < 0 ) {
+			throw new ErrorPageError(
+				"whoiswatching-not-possible-title", "whoiswatching-not-possible", [ $this->targetPage ]
+			);
+		}
+
+		return true;
+	}
+
+	protected function addWatchersForm() {
+		if ( $this->allowAddingPeople === false ) {
+			return false;
+		}
+
+		$this->getOutput()->addModules( 'mediawiki.userSuggest' );
+
+		$this->getOutput()->addHTML(
+			Html::openElement(
+				'form',
+				[ 'method' => 'post',
+				  'action' => $this->getPageTitle( $this->targetPage )->getLocalUrl(),
+				  'name' => 'uluser',
+				  'id' => 'mw-whoiswatching-form1' ]
+			) .
+			Html::hidden( 'addToken', $this->getUser()->getEditToken( __CLASS__ ) ) .
+            ( !$this->targetPage->exists() ? '<b>This page does not (yet) exist!</b>' : '' ) .
+			Xml::fieldset( $this->msg( 'whoiswatching-lookup-user' )->text() ) .
+			Xml::inputLabel(
+				$this->msg( 'whoiswatching-user-editname' )->text(),
+				'user',
+				'username',
+				30,
+				'',
+				[ 'autofocus' => true,
+				  'class' => 'mw-autocomplete-user' ] // used by mediawiki.userSuggest
+			) . ' ' .
+			Xml::submitButton( $this->msg( 'whoiswatching-adduser' )->text() ) .
+			Html::closeElement( 'fieldset' ) .
+			Html::closeElement( 'form' ) . "\n"
+		);
+
+		if ( $this->maybeAddWatcher() ) {
+			$this->uiNotifyUser();
+		}
+		return false;
+	}
+
+	protected function maybeAddWatcher() {
+		$req = $this->getRequest();
+		$token = $req->getVal( 'addToken' );
+		if ( $req->wasPosted() && $token ) {
+			if ( $this->getUser()->matchEditToken( $token, __CLASS__ ) ) {
+				$user = User::newFromName( $req->getVal( 'user' ) );
+				$title = $this->targetPage;
+				$user->addWatch( $title );
+				$this->getOutput()->redirect( $this->getPageTitle( $title )->getLocalUrl() );
+                $this->eNotifUser( 'add', $title, $user );
+				return true;
+			}
+
+			throw new ErrorPageError( 'sessionfailure-title', 'sessionfailure' );
+		}
+		return true;
+	}
+
+	protected function eNotifUser( $action, Title $title, User $user ) {
+	}
+
+	protected function uiNotifyUser( ) {
+	}
+
+	protected function pickPage() {
+		$target = $this->getRequest()->getVal( "target" );
+		if ( $target ) {
+			$this->getOutput()->redirect( $this->getPageTitle( $target )->getLocalUrl() );
+			return false;
+		}
+		$this->getOutput()->addHTML(
+			Html::openElement(
+				'form',
+				[ 'method' => 'get',
+				  'action' => $this->getPageTitle( $target )->getLocalUrl(),
+				  'name' => 'uluser',
+				  'id' => 'mw-whoiswatching-form1' ] ) .
+			Html::hidden( 'title', $this->getPageTitle()->getPrefixedText() ) .
+			Xml::fieldset( $this->msg( 'whoiswatching-lookup-title' )->text() ) .
+			Xml::inputLabel( $this->msg( 'whoiswatching-title' )->text(), 'target',
+							 'whoiswatching-target', 40,
+							 str_replace( '_', ' ', $this->targetPage ),
+							 [ 'class' => 'mw-searchInput' ] ) . ' ' .
+			Xml::submitButton( $this->msg( 'whoiswatching-select-title' )->text() ) .
+			Html::closeElement( 'fieldset' ) .
+			Html::closeElement( 'form' ) . "\n"
+		);
+		return false;
+	}
+
+	public function maybeRemoveWatcher( array $formData ) {
+		foreach ( $formData as $watcherID => $remove ) {
+			if ( $remove ) {
+				$watcher = User::newFromId( $watcherID );
+				$watcher->removeWatch( $this->targetPage );
+				$this->eNotifUser( 'remove', $this->targetPage, $watcher );
+			}
+		}
+	}
+
+	protected function showWatchingUsers() {
+		if ( $this->showWatchingUsers === false ) {
 			return;
 		}
 
-		$wgOut->addWikiText( "== ".sprintf( $this->msg( 'specialwhoiswatchingthepage' )->text(), "[[:$pageTitle]] ==" ) );
+		$out = $this->getOutput();
+		$out->addWikiText(
+			"== ". wfMessage( 'specialwhoiswatchingpage' )->params( $this->targetPage )->plain() . " =="
+		);
 
 		$dbr = wfGetDB( DB_SLAVE );
-		$watchingusers = array();
+		$watchingusers = [];
 		$res = $dbr->select(
-			'watchlist', 'wl_user', array(
-				'wl_namespace' => $pageTitle->getNamespace(),
-				'wl_title' => $pageTitle->getDBkey(),
-			), __METHOD__ );
+			'watchlist', 'wl_user', [ 'wl_namespace' => $this->targetPage->getNamespace(),
+									  'wl_title' => $this->targetPage->getDBkey() ], __METHOD__ );
 		foreach ( $res as $row ) {
 			$u = User::newFromID( $row->wl_user );
-			if ( ( $whoiswatching_nametype == 'UserName' ) || !$u->getRealName() ) {
-				$watchingusers[$row->wl_user] = ":[[User:" . $u->getName() . "]]";
-			} else {
-				$watchingusers[$row->wl_user] = ":[[:User:" . $u->getName() . "|" . $u->getRealName() . "]]";
+			$key = $u->mId;
+			$display = $u->getRealName();
+			if ( ( $this->nameType == 'UserName' ) || !$u->getRealName() ) {
+				$display = $u->getName();
 			}
+			$watchingusers[$key] = $display;
 		}
 
 		asort( $watchingusers );
-		$out = '';
-		foreach ( $watchingusers as $id => $link ) {
-			$out .= "$link\n";
-		}
-		$wgOut->addWikiText( $out );
 
-		if ( $whoiswatching_allowaddingpeople ) {
-			$wgOut->addWikiText( "== ".$this->msg( 'specialwhoiswatchingaddusers')->text()." ==" );
-			$wgOut->addHTML( "<form method=\"post\">" );
-			$wgOut->addHTML( "<input type=\"hidden\" value=\"".$wgUser->getEditToken()."\" name=\"token\" />" );
-			$wgOut->addHTML( "<div style=\"border: thin solid #000000\"><table cellpadding=\"15\" cellspacing=\"0\" border=\"0\">" );
-			$wgOut->addHTML( "<tr><td>" );
-			$wgOut->addHTML( '<select name="idArray[]" size="12" multiple="multiple">' );
-			$users = array();
-			$res = $dbr->select( 'user', 'user_name', '', __METHOD__);
-			foreach ( $res as $row ) {
-				$u = User::newFromName( $row->user_name );
-				if ( !array_key_exists( $u->getID(), $watchingusers ) &&
-					$u->isAllowed( 'read' ) && $u->getEmail() ) {
-					$users[ $u->getID() ] = $u->getRealName() ? $u->getRealName() : $u->getName();
-				}
-			}
-			asort( $users );
-			foreach ( $users as $id => $name ) {
-				$wgOut->addHTML( "<option value=\"".$id."\">".$name."</option>" );
-			}
-			$wgOut->addHTML( '</select></td><td>' );
-			$wgOut->addHTML( "<input type=\"submit\" value=\"".$this->msg( 'specialwhoiswatchingaddbtn' )->escaped()."\" />" );
-			$wgOut->addHTML( "</td></tr></table></div></form>" );
+		$users = [];
+		foreach ( $watchingusers as $id => $link ) {
+			$users[ $id ] = [ 'type' => 'check', 'label' => $link ];
 		}
+		if ( $this->allowAddingPeople ) {
+			$form = new HTMLForm( $users, $this->getContext() );
+			$form->setSubmitText( $this->msg( 'whoiswatching-deluser' )->text() );
+			$form->setSubmitCallback( [ $this, 'maybeRemoveWatcher' ] );
+			$form->show();
+		}
+	}
+
+	public static function onSkinTemplateOutputPageBeforeExec( Skin $template, QuickTemplate $tpl ) {
+		$conf = new GlobalVarConfig( "whoiswatching_" );
+		$showIfZero = $conf->get( "showifzero" );
+		$showWatchingUsers = $conf->get( "showwatchingusers" );
+
+		if (
+			RequestContext::getMain()->getOutput()->getTitle()->getNamespace() >= 0 &&
+			$showWatchingUsers
+		) {
+			$dbr = wfGetDB( DB_SLAVE );
+			$title = $template->getTitle();
+			$res = $dbr->select( 'watchlist', 'COUNT(*) as count', [
+								 'wl_namespace' => $title->getNamespace(),
+								 'wl_title' => $title->getDBkey(),
+			], __METHOD__ );
+			$watch = $dbr->fetchObject( $res );
+			if ( $watch->count > 0 || $showIfZero ) {
+				$msg = wfMessage( 'whoiswatching_users_pageview',
+					RequestContext::getMain()->getLanguage()->formatNum( $watch->count )
+				)->parse();
+				$tpl->set( 'numberofwatchingusers', $msg );
+			}
+		}
+
+		return true;
 	}
 }
