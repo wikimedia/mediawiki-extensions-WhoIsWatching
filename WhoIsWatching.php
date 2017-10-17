0122109235a6359b406fa2176d0ab41574394319
Mark A. Hershberger <mah@nichework.com>
2017-10-17 15:15:24 -0500

    Replace FooBar with WhoIsWatching
    Missed these in I3b80e90fc3df42af6112673d0d68ca9bc888363d
    
    Change-Id: Ie5321c89f7bb7bd88002cc321d4b3884f543aae9

--- WhoIsWatching.php
+++ WhoIsWatching.php
@@ -5,11 +5,11 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	$wgMessagesDirs['WhoIsWatching'] = __DIR__ . '/i18n';
 	$wgExtensionMessagesFiles['WhoIsWatchingAlias'] = __DIR__ . '/src/i18n/Alias.php';
 	wfWarn(
-		'Deprecated PHP entry point used for the FooBar extension. ' .
+		'Deprecated PHP entry point used for the WhoIsWatching extension. ' .
 		'Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
 	);
 	return;
 } else {
-	die( 'This version of the FooBar extension requires MediaWiki 1.25+' );
+	die( 'This version of the WhoIsWatching extension requires MediaWiki 1.25+' );
 }
